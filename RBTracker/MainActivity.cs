de3b8d223fab6f9af40090b60c803c6bf06e91e7
laerth <solo-d@mail.ru>
2015-04-12 19:29:06 +0600

    Первая публикация дополнения RBTracker

---
+++ RBTracker/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace RBTracker
+{
+	[Activity (Label = "RBTracker", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		SmsReader smsReader;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			smsReader = new SmsReader (this);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+			
+			button.Click += delegate {
+				//button.Text = string.Format ("{0} clicks!", count++);
+				TextView textView = FindViewById<TextView> (Resource.Id.textView1);
+				textView.Text = smsReader.GetSmsCount().ToString();
+			};
+		}
+	}
+}
+
+
---
+++ RBTracker/SmsReader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Database;
+
+namespace RBTracker
+{
+	public class SmsReader
+	{
+		readonly Activity _activity;
+
+		public SmsReader (Activity activity)
+		{
+			_activity = activity;
+		}
+
+		public int GetSmsCount()
+		{
+			int c = 0;
+			var resolver = _activity.ContentResolver;
+			ICursor cursor = resolver.Query (Android.Net.Uri.Parse("content://sms/inbox"), null, null, null, null);
+			while (cursor.MoveToNext ()) {
+				c++;
+			}
+
+			return c;
+		}
+	}
+}
+
